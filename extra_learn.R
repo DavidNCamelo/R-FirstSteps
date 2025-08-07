f783f83de598a68c9f001d87a8c220774d47a352
David Camelo <NICOLAS.CAMELO28@outlook.com>
2025-08-07 08:26:37 -0500

    Implementing package::function styling over a file, adding renv

--- extra_learn.R
+++ extra_learn.R
@@ -1,52 +1,49 @@
-library(devtools)
+# Import data
 library(statsr)
-library(dplyr)
-library(ggplot2)
-
 data(mlb11)
 
-ggplot(mlb11, aes(x=at_bats, y=runs)) +
-  geom_point() +    
-  geom_smooth(method=lm)
+ggplot2::ggplot(mlb11, ggplot2::aes(x=at_bats, y=runs)) +
+  ggplot2::geom_point() +    
+  ggplot2::geom_smooth(method=lm)
 
-mlb11 %>%
-  summarise(cor(runs, at_bats))
+mlb11 |>
+  dplyr::summarise(stats::cor(runs, at_bats))
 
 plot_ss(x = at_bats, y = runs, data = mlb11)
 
 m1 <- lm(runs ~ at_bats, data = mlb11)
-instañlsummary(m1)
+summary(m1)
 
 lm.homeruns <- lm(runs ~ homeruns, data = mlb11)
 summary(lm.homeruns)
 
-ggplot(data = mlb11, aes(x = at_bats, y = runs)) +
-  geom_point() +
-  stat_smooth(method = "lm", se = FALSE)
+ggplot2::ggplot(data = mlb11, ggplot2::aes(x = at_bats, y = runs)) +
+  ggplot2::geom_point() +
+  ggplot2::stat_smooth(method = "lm", se = FALSE)
 
-y.data <- mlb11 %>%
-  filter(at_bats == 5579) %>%
-  select(runs)
+y.data <- mlb11 |>
+  dplyr::filter(at_bats == 5579) |>
+  dplyr::select(runs)
 
 y.pred <- -2789.2429 + (0.6305 * 5579)
 
 y.data - y.pred
 
 ##{r residuals}
-ggplot(data = m1, aes(x = .fitted, y = .resid)) +
-  geom_point() +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  xlab("Fitted values") +
-  ylab("Residuals")
+ggplot2::ggplot(data = m1, ggplot2::aes(x = .fitted, y = .resid)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
+  ggplot2::xlab("Fitted values") +
+  ggplot2::ylab("Residuals")
 
 ##{r hist-res}
-ggplot(data = m1, aes(x = .resid)) +
-  geom_histogram(binwidth = 25) +
-  xlab("Residuals")
+ggplot2::ggplot(data = m1, ggplot2::aes(x = .resid)) +
+  ggplot2::geom_histogram(binwidth = 25) +
+  ggplot2::xlab("Residuals")
 
 ##{r qq-res}
-ggplot(data = m1, aes(sample = .resid)) +
-  stat_qq()
+ggplot2::ggplot(data = m1, ggplot2::aes(sample = .resid)) +
+  ggplot2::stat_qq()
 
 lm.at_bats <- lm(runs ~ at_bats, data = mlb11)
 lm.hits <- lm(runs ~ hits, data = mlb11)
@@ -95,21 +92,21 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   }
 }
 
-p1 <- ggplot(data = lm.at_bats, aes(sample = .resid)) +
-  ggtitle("lm.at_bats") + 
-  stat_qq()
+p1 <- ggplot2::ggplot(data = lm.at_bats, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.at_bats") + 
+  ggplot2::stat_qq()
 
-p2 <- ggplot(data = lm.hits, aes(sample = .resid)) +
-  ggtitle("lm.hits") + 
-  stat_qq()
+p2 <- ggplot2::ggplot(data = lm.hits, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.hits") + 
+  ggplot2::stat_qq()
 
-p3 <- ggplot(data = lm.wins, aes(sample = .resid)) +
-  ggtitle("lm.wins") + 
-  stat_qq()
+p3 <- ggplot2::ggplot(data = lm.wins, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.wins") + 
+  ggplot2::stat_qq()
 
-p4 <- ggplot(data = lm.bat_avg, aes(sample = .resid)) +
-  ggtitle("lm.bat_avg") + 
-  stat_qq()
+p4 <- ggplot2::ggplot(data = lm.bat_avg, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.bat_avg") + 
+  ggplot2::stat_qq()
 
 
 
@@ -126,17 +123,17 @@ summary(lm.new_slug)
 summary(lm.new_onbase)
 
 lm.new_onbase$coefficients
-p1 <- ggplot(data = lm.new_obs, aes(sample = .resid)) +
-  ggtitle("lm.new_obs") + 
-  stat_qq()
+p1 <- ggplot2::ggplot(data = lm.new_obs, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.new_obs") + 
+  ggplot2::stat_qq()
 
-p2 <- ggplot(data = lm.new_slug, aes(sample = .resid)) +
-  ggtitle("lm.new_slug") + 
-  stat_qq()
+p2 <- ggplot2::ggplot(data = lm.new_slug, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.new_slug") + 
+  ggplot2::stat_qq()
 
-p3 <- ggplot(data = lm.new_onbase, aes(sample = .resid)) +
-  ggtitle("lm.new_onbase") + 
-  stat_qq()
+p3 <- ggplot2::ggplot(data = lm.new_onbase, ggplot2::aes(sample = .resid)) +
+  ggplot2::ggtitle("lm.new_onbase") + 
+  ggplot2::stat_qq()
 
 
 multiplot(p1, p2, p3, cols=2)
