5d851a1835da01cb980d14f00d657ffa99d58e62
David Camelo <NICOLAS.CAMELO28@outlook.com>
2025-10-27 08:18:02 -0500

    'Porgress'

--- notation_from_r4ds/layers.R
+++ notation_from_r4ds/layers.R
@@ -10,3 +10,55 @@ ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, color = class)) +
 # Right
 ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, shape = class)) +
   ggplot2::geom_point()
+
+
+# Similarly, we can map class to size or alpha aesthetics as well,
+# which control the size and the transparency of the points, respectively.
+
+# Left
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, size = class)) +
+  ggplot2::geom_point()
+#> Warning: Using size for a discrete variable is not advised.
+
+# Right
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, alpha = class)) +
+  ggplot2::geom_point()
+#> Warning: Using alpha for a discrete variable is not advised.
+
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy)) +
+  ggplot2::geom_point(color = 'pink', shape = 2)
+
+
+# What happens if you map an aesthetic to something other than a variable name,
+#  like aes(color = displ < 5)? Note, you’ll also need to specify x and y.
+
+ggplot2::ggplot(
+  ggplot2::mpg,
+  ggplot2::aes(x = displ, y = hwy, color = displ < 5)
+) +
+  ggplot2::geom_point(shape = 2)
+
+# Rt: the result create categories about the displ values
+# if the condition it's true or false
+
+# Ways to draw smooth lines
+# Continuous and segmented
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, shape = drv)) +
+  ggplot2::geom_smooth()
+
+# Grouping and changing line type
+ggplot2::ggplot(
+  ggplot2::mpg,
+  ggplot2::aes(x = displ, y = hwy, linetype = drv)
+) +
+  ggplot2::geom_smooth()
+
+# Combining points and smooth, and even including groups
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy, color = drv)) +
+  ggplot2::geom_point() +
+  ggplot2::geom_smooth(ggplot2::aes(linetype = drv))
+
+# Cointinuous line with points and cetegories
+ggplot2::ggplot(ggplot2::mpg, ggplot2::aes(x = displ, y = hwy)) +
+  ggplot2::geom_point(ggplot2::aes(color = class)) +
+  ggplot2::geom_smooth()
